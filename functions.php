3060291ff20b25c5afc2b69e8fc07812dd7cc6e3
oleggerman7 <oleg.german7@gmail.com>
2019-01-23 17:58:39 +0200

    child theme

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+function true_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+}
+ 
+add_action( 'wp_enqueue_scripts', 'true_enqueue_styles' );
+
+function count_post(){
+global $wpdb;
+return $post_count = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->posts WHERE post_type = 'post' and post_status = 'publish';" );
+}
+//SELECT * FROM wp_posts WHERE post_type = 'post' and post_status = 'publish';
+
+?>
